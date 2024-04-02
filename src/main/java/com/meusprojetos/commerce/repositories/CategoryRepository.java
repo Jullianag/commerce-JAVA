f53315bbf84ad8a48a358084e077348091e4fe53
Julliana <juh_g@hotmail.com>
2024-04-02 14:29:20 -0300

    Correção da classe FieldMessage e criação de Category: Service,Controller e Repository

---
+++ src/main/java/com/meusprojetos/commerce/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.meusprojetos.commerce.repositories;
+
+import com.meusprojetos.commerce.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
